c897f85bd8d69b40857808b518d935c94cc5057c
siobyrne <siobyrne@users.noreply.github.com>
2023-10-13 18:06:01 -0400

    Add markerColor function

--- leaflet-part-1/static/js/logic.js
+++ leaflet-part-1/static/js/logic.js
@@ -12,3 +12,13 @@ d3.json(url).then(function (data) {
 function markerSize(mag){
     return mag * 2500;
 };
+
+// set color scale based on depth of quake
+function markerColor(depth) {
+    if (depth < 10) return "rgb(156, 223, 124)";
+    else if ( depth < 30) return "rgb(74, 189, 140)";
+    else if ( depth < 50) return "rgb(0, 150, 142)";
+    else if ( depth < 70) return "rgb(16, 110, 124)";
+    else if ( depth < 90) return "rgb(42, 72, 88)";
+    else return "rgb(250, 250, 110)";
+};
