eb0d499cd356ecb1498a9edb1c79bb1341972bd6
siobyrne <siobyrne@users.noreply.github.com>
2023-10-14 12:41:50 -0400

    Add geoJSON function and marker selection

--- leaflet-part-1/static/js/logic.js
+++ leaflet-part-1/static/js/logic.js
@@ -22,3 +22,31 @@ function markerColor(depth) {
     else if ( depth < 90) return "rgb(42, 72, 88)";
     else return "rgb(250, 250, 110)";
 };
+
+function createFeatures(quakedata){
+
+    // create a function that gets each feature and creates a popup of time/place of earthquake
+    function eachFeature(feature, layer){
+        layer.bindPopup(`<h3>Place: ${feature.properties.place}</h3><hr><p>Time: ${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+    }
+
+    // create geoJSON layer and run  eachFeature
+
+    let quakes = L.geoJSON(quakedata, {
+        onEachFeature: eachFeature,
+
+        // tell layer to use bubble marker via pointToLayer
+        pointToLayer: function(feature, location){
+
+            var markers = {
+                radius: markerSize(feature.properties.mag),
+            fillColor: markerColor(feature.geometry.coordinates[2]),
+            color: "black",
+            stroke: true,
+            fillOpacity: 0.75,
+            weight: 1
+            }
+            return L.circle(location, markers)
+        }
+    });
+}
